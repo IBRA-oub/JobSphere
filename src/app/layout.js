897d8eeb2cf6db10ec2644f6ee3ee0c300309792
IBRA-oub <brahimbourih@gmail.com>
2024-12-27 09:51:54 +0100

    JOBS-50 feat : création de la page home

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import localFont from "next/font/local";
 import "./globals.css";
+import Link from "next/link";
+import Image from "next/image";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -23,6 +25,31 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+
+        <nav className="w-full h-16 bg-black text-white flex justify-around items-center ">
+          <div>
+            <p>
+              <Image
+                src="/images/jobLogo-remove.png"
+                alt="Logo"
+                width={90}
+                height={90}
+              />
+              
+            </p>
+          </div>
+          <div className="flex justify-between w-96  text-gray-400 ">
+            <Link href="/" className="hover:text-white">Home </Link>
+            <Link href="/" className="hover:text-white">Jobs </Link>
+            <Link href="/" className="hover:text-white">About Us </Link>
+            <Link href="/" className="hover:text-white">Contact Us </Link>
+          </div>
+          <div className="w-32 flex justify-between">
+            <button className="mr-4 hover:drop-shadow-2xl">Login </button>
+            <button className="py-2 rounded-md px-5 bg-[#309689] hover:bg-[#298377] hover:shadow-sm hover:shadow-white transition duration-500 ease-in-out">Register</button>
+          </div>
+        </nav>
+
         {children}
       </body>
     </html>
--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,19 @@
 import Image from "next/image";
+import HeroSection from "./components/home/heroSection";
+import CollabSection from "./components/home/collabSection";
+import RecentJobSection from "./components/home/recentJobSection";
+import Testimonials from "./components/home/testimonials";
+import Footer from "./components/home/footer";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1>
-        hello
-      </h1>
-      <p className=""></p>
-    </div>
+    <>
+      <HeroSection />
+      <CollabSection />
+      <RecentJobSection/>
+      <Testimonials/>
+      <Footer/>
+    </>
+
   );
 }
